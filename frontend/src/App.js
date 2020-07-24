a09b5427a7a44f4df625a491c69b2c0f2611b0af
Nil <nil.mamano@gmail.com>
2020-07-24 00:50:04 -0500

    Move hardcoded backend endpoint into environment file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ import socketIoClient from "socket.io-client";
 import LobbyPage from "./lobby/LobbyPage";
 
 const App = () => {
-  const BACKEND_ENDPOINT = "localhost:4001"; //placeholder
+  const BACKEND_ENDPOINT =
+    process.env.REACT_APP_BACKEND_URL || "localhost:4001";
   const [socket] = useState(socketIoClient(BACKEND_ENDPOINT));
 
   //every route redirects back to '/', which is both the lobby and the game page
