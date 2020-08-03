22dd3773eaf449d756788eb94574133b8980e90b
Nil <nil.mamano@gmail.com>
2020-08-03 05:18:56 -0500

    Use cookies to store last used player name and time control

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import socketIoClient from "socket.io-client";
+import { CookiesProvider } from "react-cookie";
 
 import LobbyPage from "./lobby/LobbyPage";
 
@@ -10,17 +11,20 @@ const App = () => {
   console.log(`connecting to backend at ${BACKEND_ENDPOINT}`);
   const [socket] = useState(socketIoClient(BACKEND_ENDPOINT));
 
-  //every route redirects back to '/', which is both the lobby and the game page
+  //every route redirects back to '/wallwars/index.html',
+  //where the entire app exists as a single-page application
   return (
     <React.StrictMode>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/wallwars/index.html" exact>
-            <LobbyPage socket={socket} />
-          </Route>
-          <Redirect to="/wallwars/index.html" />
-        </Switch>
-      </BrowserRouter>
+      <CookiesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/wallwars/index.html" exact>
+              <LobbyPage socket={socket} />
+            </Route>
+            <Redirect to="/wallwars/index.html" />
+          </Switch>
+        </BrowserRouter>
+      </CookiesProvider>
     </React.StrictMode>
   );
 };
--- frontend/src/lobby/LobbyPage.js
+++ frontend/src/lobby/LobbyPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { uniqueNamesGenerator, names } from "unique-names-generator";
 import { useMediaQuery } from "react-responsive";
 import { ToastContainer } from "react-toastify";
-
+import { useCookies } from "react-cookie";
 import GamePage from "../game/GamePage";
 import Header from "../shared/Header";
 import LobbyForm from "./LobbyForm";
@@ -18,9 +18,20 @@ const randPlayerName = () =>
   }).slice(0, maxPlayerNameLen);
 
 const LobbyPage = ({ socket }) => {
-  const [playerName, setPlayerName] = useState(randPlayerName());
-  const [duration, setDuration] = useState(5);
-  const [increment, setIncrement] = useState(5);
+  //the player name is saved in a cookie when the player uses a name
+  //to create or join a game. the time control is saved when the player
+  //creates a game
+  const [cookies, setCookie] = useCookies([
+    "playerName",
+    "duration",
+    "increment",
+  ]);
+
+  const [playerName, setPlayerName] = useState(
+    cookies.playerName || randPlayerName()
+  );
+  const [duration, setDuration] = useState(cookies.duration || 5);
+  const [increment, setIncrement] = useState(cookies.increment || 5);
   const [joinGameId, setJoinGameId] = useState("");
   const [isOngoingGame, setIsOngoingGame] = useState(false);
   const [creatorParams, setCreatorParams] = useState(null);
@@ -42,24 +53,34 @@ const LobbyPage = ({ socket }) => {
     if (isNaN(dur) || dur < 0.1) {
       dur = 5 / 60;
       showToastNotification("Invalid duration, using 5s instead", 5000);
+    } else {
+      setCookie("duration", dur, { path: "/" });
     }
     if (isNaN(inc) || inc < 0) {
       inc = 0;
       showToastNotification("Invalid increment, using 0s instead", 5000);
+    } else {
+      setCookie("increment", inc, { path: "/" });
     }
+    let name = playerName;
+    if (name === "") name = "Anon";
+    else setCookie("playerName", name, { path: "/" });
     setCreatorParams({
       timeControl: {
         duration: dur,
         increment: inc,
       },
-      creatorName: playerName === "" ? "Anon" : playerName,
+      creatorName: name,
     });
     setIsOngoingGame(true);
   };
   const handleJoinGame = () => {
+    let name = playerName;
+    if (name === "") name = "Anon";
+    else setCookie("playerName", name, { path: "/" });
     setJoinerParams({
       gameId: joinGameId,
-      joinerName: playerName === "" ? "Anon" : playerName,
+      joinerName: name,
     });
     setIsOngoingGame(true);
   };
