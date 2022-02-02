aa79a99b0c2ec3a317195dd68f9201cad220a1ba
Nil <nil.mamano@gmail.com>
2022-02-01 21:27:01 -0800

    AI: fix a bug in the move choice when the AI is lost.

--- AI/negamaxer.cc
+++ AI/negamaxer.cc
@@ -109,12 +109,14 @@ Move Negamaxer::GetMove(Situation sit) {
   if (best_move_eval != -kInfinity) return best_move;
 
   // If the game is lost, return the move that moves towards the goal.
-  int cur_dist = sit_.G.Distance(sit_.tokens[sit_.turn], kGoals[sit_.turn]);
+  int turn = sit_.turn;
+  int cur_dist = sit_.G.Distance(sit_.tokens[turn], kGoals[turn]);
   for (const ScoredMove& scored_move : OrderedMoves(kMaxDepth - 1)) {
     const Move& move = scored_move.move;
     sit_.ApplyMove(move);
-    int new_dist = sit_.G.Distance(sit_.tokens[sit_.turn], kGoals[sit_.turn]);
+    int new_dist = sit_.G.Distance(sit_.tokens[turn], kGoals[turn]);
     if (new_dist == cur_dist - 2) return move;
+    sit_.UndoMove(move);
   }
   // The program cannot reach here, since there is always a move that goes 2
   // steps closer to the goal.
