8a8b0b607778c35179f7ca756665fbf8d4dbeef9
Nil <nil.mamano@gmail.com>
2022-02-21 09:18:13 -0800

    AI: use cmake presets instead of toolchains.

--- AI/cmake/toolchain-gcc.cmake
+++
@@ -1,5 +0,0 @@
-set(CMAKE_CXX_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic")
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Ofast -march=haswell")
