aa48b8acd07353bbfe165f84fa57623bdad628c6
Nil <nil.mamano@gmail.com>
2022-04-11 23:26:11 -0700

    AI: Parse standard notation to construct situations.

--- AI/source/situation.cc
+++ AI/source/situation.cc
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "constants.h"
@@ -13,6 +14,218 @@
 Situation::Situation()
     : tokens(
           {static_cast<int8_t>(kStarts[0]), static_cast<int8_t>(kStarts[1])}) {}
+
+namespace {
+
+bool IsWhiteSpace(char c) { return c == ' ' || c == '\t' || c == '\n'; }
+
+// Advances `s_i` while it is at a white space character.
+void SkipWhiteSpace(const std::string& s, size_t& s_i) {
+  while (s_i < s.size() && IsWhiteSpace(s[s_i])) ++s_i;
+}
+
+void PrintStringWithPointer(const std::string& s, size_t s_i) {
+  bool single_line_fits = s.size() < 80 && s.find('\n') == std::string::npos;
+  if (single_line_fits) {
+    std::cout << s << std::endl;
+    std::string space(s_i, ' ');
+    std::cout << space << '^' << std::endl;
+  } else {
+    std::cout << s.substr(0, s_i) << '#' << s.substr(s_i) << std::endl;
+  }
+}
+// Advances `s_i`, an index in `s`, until after `token`. It skips white spaces
+// before `token`. Returns false if anything other than white spaces followed
+// by `token` is encountered, or if `s` ends before the token is found.
+bool ConsumeToken(const std::string& token, const std::string& s, size_t& s_i) {
+  SkipWhiteSpace(s, s_i);
+  if (s.find(token, s_i) != s_i) {
+    std::cout << "Could not parse token '" << token << "'" << std::endl;
+    PrintStringWithPointer(s, s_i);
+    return false;
+  }
+  s_i += token.size();
+  return true;
+}
+
+// Returns whether there is only white space in `s` at and after `s[s_i]`.
+// Advances `s_i` over white space.
+bool IsDoneParsing(const std::string& s, size_t& s_i) {
+  SkipWhiteSpace(s, s_i);
+  return s_i == s.size();
+}
+
+struct ParsedAction {
+  int col, row;  // 0-indexed.
+  char type;     // '.' for move, 'v' for wall below, '>' for wall to the right.
+};
+
+struct ParsedMove {
+  std::vector<ParsedAction> actions;
+};
+
+bool IsValidColumnLetter(char c) {
+  c = tolower(c);
+  return c >= 'a' && c < 'a' + kNumCols;
+}
+
+bool ParseColumnLetter(const std::string& s, size_t& s_i, int& col) {
+  if (s_i == s.size() || !IsValidColumnLetter(s[s_i])) {
+    std::cout << "Could not parse column letter" << std::endl;
+    PrintStringWithPointer(s, s_i);
+    return false;
+  }
+  col = tolower(s[s_i]) - 'a';
+  ++s_i;
+  return true;
+}
+
+bool ParseRowNumber(const std::string& s, size_t& s_i, int& row) {
+  if (s_i == s.size()) {
+    std::cout << "Could not parse row number" << std::endl;
+    PrintStringWithPointer(s, s_i);
+    return false;
+  }
+  char c = tolower(s[s_i]);
+  if (c == 'x') {
+    if (kNumRows < 10) {
+      std::cout << "Could not parse row number" << std::endl;
+      PrintStringWithPointer(s, s_i);
+      return false;
+    }
+    row = 9;
+  } else if (c >= '1' && c <= '9' && c < '1' + kNumRows) {
+    row = c - '1';
+  } else {
+    std::cout << "Could not parse row number" << std::endl;
+    PrintStringWithPointer(s, s_i);
+    return false;
+  }
+  ++s_i;
+  return true;
+}
+
+bool IsDoneParsingMove(const std::string& s, size_t& s_i) {
+  SkipWhiteSpace(s, s_i);
+  return s_i == s.size() || !IsValidColumnLetter(s[s_i]);
+}
+
+bool ParseAction(const std::string& s, size_t& s_i, ParsedAction& action) {
+  SkipWhiteSpace(s, s_i);
+  if (!ParseColumnLetter(s, s_i, action.col)) return false;
+  if (!ParseRowNumber(s, s_i, action.row)) return false;
+  if (s_i < s.size()) {
+    char c = tolower(s[s_i]);
+    if (c == 'v' || c == '>') {
+      action.type = c;
+      ++s_i;
+    } else {
+      action.type = '.';
+    }
+  }
+  return true;
+}
+
+// Parses a move in standard notation starting at s[s_i]. Returns false if
+// parsing fails. Otherwise, returns true, moves i to after the parsed string
+// and stores the parsed move in `move`. Any initial white space is skipped. If
+// there is more than one action, any order of the actions is accepted. Upper
+// and lowercase letters are allowed. Examples of moves: "a1", "lX", "lX a1v",
+// "a1v a1>", a2> l1>", "A2> DxV".
+bool ParseMove(const std::string& s, size_t& s_i, ParsedMove& move) {
+  int walk_action_count = 0;
+  for (int action_index = 0; !IsDoneParsingMove(s, s_i); ++action_index) {
+    if (action_index == 2) {
+      std::cout << "Found move with more than 2 actions while parsing"
+                << std::endl;
+      PrintStringWithPointer(s, s_i);
+      return false;
+    }
+    SkipWhiteSpace(s, s_i);
+    size_t original_s_i = s_i;
+    ParsedAction action;
+    if (!ParseAction(s, s_i, action)) return false;
+    move.actions.push_back(action);
+    if (action.type == '.') ++walk_action_count;
+    if (walk_action_count > 1) {
+      std::cout << "Found move with more than 1 walk action while parsing"
+                << std::endl;
+      PrintStringWithPointer(s, original_s_i);
+      return false;
+    }
+  }
+  return true;
+}
+
+// Parses a string in standard notation into the list of individual moves.
+// Returns whether parsing succeeds, in which case `parsed_moves` contains the
+// list of moves and `notated_moves` contains the list of strings corresponding
+// to the moves.
+bool ParseMoveList(const std::string& s, std::vector<ParsedMove>& parsed_moves,
+                   std::vector<std::string>& notated_moves) {
+  size_t s_i = 0;
+  for (int move_index = 1; !IsDoneParsing(s, s_i); ++move_index) {
+    std::string move_number = std::to_string(move_index) + ".";
+    if (!ConsumeToken(move_number, s, s_i)) return false;
+    ParsedMove parsed_move;
+    SkipWhiteSpace(s, s_i);
+    size_t original_s_i = s_i;
+    if (!ParseMove(s, s_i, parsed_move)) return false;
+    parsed_moves.push_back(parsed_move);
+    notated_moves.push_back(s.substr(original_s_i, s_i - original_s_i - 1));
+  }
+  return true;
+}
+
+Move ParsedMoveToMove(ParsedMove parsed_move, const Situation& sit) {
+  int to_node = 0;
+  std::vector<int> edges;
+  for (const ParsedAction& parsed_action : parsed_move.actions) {
+    int node = NodeAt(parsed_action.row, parsed_action.col);
+    if (parsed_action.type == '.') {
+      to_node = node;
+    } else if (parsed_action.type == 'v') {
+      edges.push_back(EdgeBelow(node));
+    } else /*parsed_action.type == '>'*/ {
+      edges.push_back(EdgeRight(node));
+    }
+  }
+  Move move;
+  if (edges.size() == 0) {
+    move = DoubleWalkMove(sit.tokens[sit.turn], to_node);
+  } else if (edges.size() == 1) {
+    move = WalkAndBuildMove(sit.tokens[sit.turn], to_node, edges[0]);
+  } else /*edges.size() == 2*/ {
+    move = DoubleBuildMove(edges[0], edges[1]);
+  }
+  return move;
+}
+
+}  // namespace
+
+bool Situation::BuildFromStandardNotationMoves(const std::string& s,
+                                               Situation& sit) {
+  sit = {};
+  std::vector<ParsedMove> parsed_moves;
+  std::vector<std::string> notated_moves;
+  if (!ParseMoveList(s, parsed_moves, notated_moves)) return false;
+
+  for (size_t i = 0; i < parsed_moves.size(); ++i) {
+    ParsedMove parsed_move = parsed_moves[i];
+    Move move = ParsedMoveToMove(parsed_move, sit);
+    if (!sit.IsLegalMove(move)) {
+      std::cout << "Could not apply move " << i + 1 << ": <" << notated_moves[i]
+                << "> {" << sit.MoveToString(move) << "} [" << move << "] ("
+                << parsed_move.actions[0].row << ", "
+                << parsed_move.actions[0].col << ") to situation" << std::endl
+                << sit;
+      sit = {};
+      return false;
+    }
+    sit.ApplyMove(move);
+  }
+  return true;
+}
 
 void Situation::ApplyMove(Move move) {
   DBGS(CrashIfMoveIsIllegal(move));
