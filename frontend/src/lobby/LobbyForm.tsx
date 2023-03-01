0c8bcd049b8dfae338e392cb191af0f64a218721
Nil <nil.mamano@gmail.com>
2023-02-28 23:53:58 -0800

    Move text input fields out of react-materialize library.

--- frontend/src/lobby/LobbyForm.tsx
+++ frontend/src/lobby/LobbyForm.tsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import {
   Row,
   Col,
-  TextInput,
   Button,
   Icon,
   Checkbox,
@@ -19,6 +18,7 @@ import BoardSizeSlider from "../shared/BoardSizeSlider";
 import { eloIdAboutText } from "./lobbyHelp";
 import { ClientParams, PosSetting } from "./LobbyPage";
 import TokenDropdown from "./TokenDropdown";
+import TextInputField from "../shared/TextInputField";
 
 export default function LobbyForm({
   clientParams,
@@ -96,6 +96,8 @@ export default function LobbyForm({
   const [tokenDropdownAnchorEl, setTokenDropdownAnchorEl] =
     React.useState<null | HTMLElement>(null);
 
+  // todo: the isInvalid prop in TextInputField could be used to highlight invalid inputs.
+
   return (
     <div
       className="container"
@@ -112,13 +114,14 @@ export default function LobbyForm({
           </span>
         </Col>
         <Col s={6} m={3}>
-          <TextInput
-            id="nameInput"
-            value={clientParams.playerName}
-            onChange={(props) => {
-              handlePlayerName(props.target.value);
-            }}
-          />
+          <div style={{ paddingTop: "15px" }}>
+            <TextInputField
+              label=""
+              id="nameInput"
+              value={clientParams.playerName}
+              onChange={handlePlayerName}
+            />
+          </div>
         </Col>
         <Col s={1} m={1}>
           <Button
@@ -147,26 +150,22 @@ export default function LobbyForm({
           />
         </Col>
         <Col s={2} m={2} style={{ paddingRight: "0" }}>
-          <TextInput
-            id="durationInput"
+          <TextInputField
             label="Duration"
+            id="durationInput"
             value={`${clientParams.timeControl.duration}`}
-            onChange={(props) => {
-              handleDuration(props.target.value);
-            }}
+            onChange={handleDuration}
           />
         </Col>
         <Col s={1} m={1} style={{ paddingLeft: "0" }}>
           m
         </Col>
         <Col s={2} m={2} style={{ paddingRight: "0" }}>
-          <TextInput
-            id="incrementInput"
+          <TextInputField
             label="Increment"
+            id="incrementInput"
             value={`${clientParams.timeControl.increment}`}
-            onChange={(props) => {
-              handleIncrement(props.target.value);
-            }}
+            onChange={handleIncrement}
           />
         </Col>
         <Col s={1} m={1} style={{ paddingLeft: "0" }}>
@@ -245,17 +244,16 @@ export default function LobbyForm({
         </Row>
       )}
       {showMoreOptions && (
-        <Row className="valign-wrapper">
+        <Row className="valign-wrapper" style={{ paddingBottom: "15px" }}>
           <Col className="center" s={4} m={4}>
             <span style={{ fontSize: "23px" }}>ELO id:</span>
           </Col>
           <Col s={6} m={3}>
-            <TextInput
+            <TextInputField
+              label=""
               id="eloIdInput"
               value={clientParams.eloId}
-              onChange={(props) => {
-                handleEloId(props.target.value);
-              }}
+              onChange={handleEloId}
             />
           </Col>
           <Col s={1} m={1}>
@@ -482,14 +480,12 @@ export default function LobbyForm({
             />
           </Col>
           <Col s={6} m={5}>
-            <TextInput
+            <TextInputField
               id="joinInput"
-              placeholder="Write game code here..."
+              label=""
               value={`${clientParams.joinCode}`}
-              onChange={(props) => {
-                console.log(props);
-                handleJoinCode(props.target.value);
-              }}
+              onChange={handleJoinCode}
+              placeholder="Write game code here..."
             />
           </Col>
           <Col s={1} m={3}></Col>
