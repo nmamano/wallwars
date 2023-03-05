98797366a6f9ea02f23a3b28d1b89533bb728d13
Nil <nil.mamano@gmail.com>
2023-03-04 17:06:02 -0800

    Remove dead code and dependencies.

--- frontend/src/shared/FormSlider.tsx
+++ frontend/src/shared/FormSlider.tsx
@@ -1,54 +1,10 @@
 import * as React from "react";
-import Nouislider from "nouislider-react";
-import "nouislider/distribute/nouislider.css";
-import wNumb from "wnumb";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Slider from "@mui/material/Slider";
 import MuiInput from "@mui/material/Input";
-
-export function OldFormSlider({
-  label,
-  min,
-  max,
-  value,
-  onChange,
-  internalToDisplay,
-  displayToInternal,
-}: {
-  label: string;
-  min: number;
-  max: number;
-  value: number;
-  onChange: (value: number) => void;
-  internalToDisplay: (internalCoord: number) => number;
-  displayToInternal: (classicCoord: number) => number;
-}): JSX.Element {
-  return (
-    <div>
-      <span style={{ fontSize: "15px" }}>{label}</span>
-      <Nouislider
-        style={{
-          marginBottom: "12px",
-        }}
-        range={{
-          min: internalToDisplay(min),
-          max: internalToDisplay(max),
-        }}
-        start={[internalToDisplay(value)]}
-        step={1}
-        tooltips={[wNumb({ decimals: 0 })]}
-        // connect="lower" // Removed until we can change the color.
-        // @ts-ignore
-        onChange={(value: number) => {
-          onChange(displayToInternal(value));
-        }}
-      />
-    </div>
-  );
-}
 
 const Input = styled(MuiInput)`
   width: 42px;
